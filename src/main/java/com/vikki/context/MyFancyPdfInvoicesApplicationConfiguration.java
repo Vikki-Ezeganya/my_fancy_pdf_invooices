2c2f08f0ff11ce8e0bdb4bf0db0d130c70b09268
Vikki <vikkiezeganya@gmail.com>
2022-01-30 23:59:12 +0100

    Added constructor DI

--- src/main/java/com/vikki/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/vikki/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -2,8 +2,6 @@ package com.vikki.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vikki.App;
-import com.vikki.service.InvoiceService;
-import com.vikki.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,16 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackageClasses = App.class)
 public class MyFancyPdfInvoicesApplicationConfiguration {
-
-//    @Bean
-//    public UserService userService() {
-//        return new UserService();
-//    }
-//
-//    @Bean
-//    public InvoiceService invoiceService(UserService userService) {
-//        return new InvoiceService(userService);
-//    }
 
     @Bean
     public ObjectMapper ObjectMapper() {
--- src/main/java/com/vikki/service/InvoiceService.java
+++ src/main/java/com/vikki/service/InvoiceService.java
@@ -10,13 +10,14 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Component
 public class InvoiceService {
 
-   private  List<Invoice>  invoices;
-   private final UserService userService;
+       private  List<Invoice>  invoices;
 
-   public InvoiceService(UserService userService) {
-       this.userService = userService;
-       this.invoices  = new CopyOnWriteArrayList<>();
-    }
+       private final UserService userService;
+
+       public InvoiceService(UserService userService) {
+            this.userService = userService;
+            this.invoices  = new CopyOnWriteArrayList<>();
+       }
 
         public Invoice create(String userId, Integer amount) {
             User user = userService.findById(userId);
@@ -27,14 +28,11 @@ public class InvoiceService {
 
             String pdfUrl = "http://www.africau.edu/images/default/sample.pdf";
             Invoice invoice = new Invoice(userId, amount, pdfUrl );
-            System.out.println(invoice);
             invoices.add(invoice);
             return invoice;
         }
 
         public  List<Invoice> findAll() {
-            System.out.println("Hello");
-            System.out.println(invoices);
             return invoices;
         }
 
