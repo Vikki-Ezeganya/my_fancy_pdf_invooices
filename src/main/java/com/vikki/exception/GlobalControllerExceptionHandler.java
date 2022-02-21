080bfcc02fac73045acd59f490f9966210451f60
Vikki <vikkiezeganya@gmail.com>
2022-02-21 23:59:43 +0100

    added response status on exception handler

--- src/main/java/com/vikki/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/vikki/exception/GlobalControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.vikki.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,11 +11,13 @@ import javax.validation.ConstraintViolationException;
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
         return "Sorry, that was not quite right: " + exception.getMessage();
     }
 
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleConstraintViolation(ConstraintViolationException exception) {
         return "Sorry, that was not quite right: " +  exception.getMessage();
     }
