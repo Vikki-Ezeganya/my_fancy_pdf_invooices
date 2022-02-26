bb91ac54b478f6a9a35cb9d8092919ce33050f27
Vikki <vikkiezeganya@gmail.com>
2022-02-27 00:01:01 +0100

    added user to thymleaf

--- src/main/java/com/vikki/web/WebsiteController.java
+++ src/main/java/com/vikki/web/WebsiteController.java
@@ -1,13 +1,20 @@
 package com.vikki.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class WebsiteController {
 
     @GetMapping("/")
-    public String homePage() {
+    public String homePage(Model model, @RequestParam(required = false, defaultValue = "stranger") String username)  {
+
+        model.addAttribute("username",  username);
+        model.addAttribute("currentDate", new Date());
         return "index.html";
     }
 }
